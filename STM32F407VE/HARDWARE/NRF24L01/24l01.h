515837574e35f21a4ca17cd64ed3c8bbaf29bf0c
Zifeng Zhao(22942831) <89691293+NOanxiety@users.noreply.github.com>
2021-10-06 10:18:58 +0800

    Update 24l01.h

--- STM32F407VE/HARDWARE/NRF24L01/24l01.h
+++ STM32F407VE/HARDWARE/NRF24L01/24l01.h
@@ -1,93 +1,93 @@
 #ifndef __24L01_H
 #define __24L01_H
 #include "sys.h"
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
-//NRF24L01寄存器操作命令
-#define NRF_READ_REG    0x00  //读配置寄存器,低5位为寄存器地址
-#define NRF_WRITE_REG   0x20  //写配置寄存器,低5位为寄存器地址
-#define RD_RX_PLOAD     0x61  //读RX有效数据,1~32字节
-#define WR_TX_PLOAD     0xA0  //写TX有效数据,1~32字节
-#define FLUSH_TX        0xE1  //清除TX FIFO寄存器.发射模式下用
-#define FLUSH_RX        0xE2  //清除RX FIFO寄存器.接收模式下用
-#define REUSE_TX_PL     0xE3  //重新使用上一包数据,CE为高,数据包被不断发送.
-#define NOP             0xFF  //空操作,可以用来读状态寄存器
-//SPI(NRF24L01)寄存器地址
-#define CONFIG          0x00  //配置寄存器地址;bit0:1接收模式,0发射模式;bit1:电选择;bit2:CRC模式;bit3:CRC使能;
-                              //bit4:中断MAX_RT(达到最大重发次数中断)使能;bit5:中断TX_DS使能;bit6:中断RX_DR使能
-#define EN_AA           0x01  //使能自动应答功能  bit0~5,对应通道0~5
-#define EN_RXADDR       0x02  //接收地址允许,bit0~5,对应通道0~5
-#define SETUP_AW        0x03  //设置地址宽度(所有数据通道):bit1,0:00,3字节;01,4字节;02,5字节;
-#define SETUP_RETR      0x04  //建立自动重发;bit3:0,自动重发计数器;bit7:4,自动重发延时 250*x+86us
-#define RF_CH           0x05  //RF通道,bit6:0,工作通道频率;
-#define RF_SETUP        0x06  //RF寄存器;bit3:传输速率(0:1Mbps,1:2Mbps);bit2:1,发射功率;bit0:低噪声放大器增益
-#define STATUS          0x07  //状态寄存器;bit0:TX FIFO满标志;bit3:1,接收数据通道号(最大:6);bit4,达到最多次重发
-                              //bit5:数据发送完成中断;bit6:接收数据中断;
-#define MAX_TX  		0x10  //达到最大发送次数中断
-#define TX_OK   		0x20  //TX发送完成中断
-#define RX_OK   		0x40  //接收到数据中断
+///////////////////////////////////////////////// ///////////////////////////////////////////////// //////
+//NRF24L01 register operation command
+#define NRF_READ_REG 0x00 //Read configuration register, the lower 5 bits are the register address
+#define NRF_WRITE_REG 0x20 //Write the configuration register, the lower 5 bits are the register address
+#define RD_RX_PLOAD 0x61 //Read RX valid data, 1~32 bytes
+#define WR_TX_PLOAD 0xA0 //Write TX valid data, 1~32 bytes
+#define FLUSH_TX 0xE1 //Clear the TX FIFO register. Used in transmit mode
+#define FLUSH_RX 0xE2 //Clear RX FIFO register. Used in receive mode
+#define REUSE_TX_PL 0xE3 //Reuse the last packet of data, CE is high, and the data packet is continuously sent.
+#define NOP 0xFF //No operation, can be used to read the status register
+//SPI(NRF24L01) register address
+#define CONFIG 0x00 //Configuration register address; bit0: 1 receiving mode, 0 transmitting mode; bit1: electrical selection; bit2: CRC mode; bit3: CRC enable;
+                              //bit4: interrupt MAX_RT (maximum number of retransmission interrupts) enable; bit5: interrupt TX_DS enable; bit6: interrupt RX_DR enable
+#define EN_AA 0x01 //Enable automatic response function bit0~5, corresponding to channel 0~5
+#define EN_RXADDR 0x02 //Receive address is allowed, bit0~5, corresponding to channel 0~5
+#define SETUP_AW 0x03 //Set the address width (all data channels): bit1,0: 00, 3 bytes; 01, 4 bytes; 02, 5 bytes;
+#define SETUP_RETR 0x04 //Establish automatic retransmission; bit3:0, automatic retransmission counter; bit7:4, automatic retransmission delay 250*x+86us
+#define RF_CH 0x05 //RF channel, bit6:0, working channel frequency;
+#define RF_SETUP 0x06 //RF register; bit3: transmission rate (0:1Mbps, 1:2Mbps); bit2:1, transmit power; bit0: low noise amplifier gain
+#define STATUS 0x07 //Status register; bit0: TX FIFO full flag; bit3:1, receiving data channel number (maximum: 6); bit4, reaching the maximum number of retransmissions
+                              //bit5: data transmission complete interrupt; bit6: receive data interrupt;
+#define MAX_TX 0x10 // Interrupt when the maximum number of transmissions is reached
+#define TX_OK 0x20 //TX transmission complete interrupt
+#define RX_OK 0x40 //Data received interrupt
 
-#define OBSERVE_TX      0x08  //发送检测寄存器,bit7:4,数据包丢失计数器;bit3:0,重发计数器
-#define CD              0x09  //载波检测寄存器,bit0,载波检测;
-#define RX_ADDR_P0      0x0A  //数据通道0接收地址,最大长度5个字节,低字节在前
-#define RX_ADDR_P1      0x0B  //数据通道1接收地址,最大长度5个字节,低字节在前
-#define RX_ADDR_P2      0x0C  //数据通道2接收地址,最低字节可设置,高字节,必须同RX_ADDR_P1[39:8]相等;
-#define RX_ADDR_P3      0x0D  //数据通道3接收地址,最低字节可设置,高字节,必须同RX_ADDR_P1[39:8]相等;
-#define RX_ADDR_P4      0x0E  //数据通道4接收地址,最低字节可设置,高字节,必须同RX_ADDR_P1[39:8]相等;
-#define RX_ADDR_P5      0x0F  //数据通道5接收地址,最低字节可设置,高字节,必须同RX_ADDR_P1[39:8]相等;
-#define TX_ADDR         0x10  //发送地址(低字节在前),ShockBurstTM模式下,RX_ADDR_P0与此地址相等
-#define RX_PW_P0        0x11  //接收数据通道0有效数据宽度(1~32字节),设置为0则非法
-#define RX_PW_P1        0x12  //接收数据通道1有效数据宽度(1~32字节),设置为0则非法
-#define RX_PW_P2        0x13  //接收数据通道2有效数据宽度(1~32字节),设置为0则非法
-#define RX_PW_P3        0x14  //接收数据通道3有效数据宽度(1~32字节),设置为0则非法
-#define RX_PW_P4        0x15  //接收数据通道4有效数据宽度(1~32字节),设置为0则非法
-#define RX_PW_P5        0x16  //接收数据通道5有效数据宽度(1~32字节),设置为0则非法
-#define NRF_FIFO_STATUS 0x17  //FIFO状态寄存器;bit0,RX FIFO寄存器空标志;bit1,RX FIFO满标志;bit2,3,保留
-                              //bit4,TX FIFO空标志;bit5,TX FIFO满标志;bit6,1,循环发送上一数据包.0,不循环;
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
-//24L01操作线
-#define NRF24L01_CE     PDout(5)  //24L01片选信号
-#define NRF24L01_CSN    PDout(6)  //SPI片选信号
-#define NRF24L01_IRQ    PDin(7)   //IRQ主机数据输入
+#define OBSERVE_TX 0x08 //Transmission detection register, bit7:4, packet loss counter; bit3:0, retransmission counter
+#define CD 0x09 //Carrier detection register, bit0, carrier detection;
+#define RX_ADDR_P0 0x0A //Data channel 0 receiving address, maximum length 5 bytes, low byte first
+#define RX_ADDR_P1 0x0B //Data channel 1 receiving address, maximum length is 5 bytes, low byte first
+#define RX_ADDR_P2 0x0C //Data channel 2 receiving address, the lowest byte can be set, and the high byte must be equal to RX_ADDR_P1[39:8];
+#define RX_ADDR_P3 0x0D //Data channel 3 receiving address, the lowest byte can be set, and the high byte must be equal to RX_ADDR_P1[39:8];
+#define RX_ADDR_P4 0x0E //Data channel 4 receiving address, the lowest byte can be set, and the high byte must be equal to RX_ADDR_P1[39:8];
+#define RX_ADDR_P5 0x0F //Data channel 5 receiving address, the lowest byte can be set, and the high byte must be equal to RX_ADDR_P1[39:8];
+#define TX_ADDR 0x10 //Send address (low byte first), in ShockBurstTM mode, RX_ADDR_P0 is equal to this address
+#define RX_PW_P0 0x11 //The effective data width of receiving data channel 0 (1~32 bytes), set to 0 is illegal
+#define RX_PW_P1 0x12 //The effective data width of receiving data channel 1 (1~32 bytes), set to 0 is illegal
+#define RX_PW_P2 0x13 //Receive data channel 2 effective data width (1~32 bytes), set to 0 is illegal
+#define RX_PW_P3 0x14 //Receive data channel 3 effective data width (1~32 bytes), set to 0 is illegal
+#define RX_PW_P4 0x15 //Receive data channel 4 effective data width (1~32 bytes), set to 0 is illegal
+#define RX_PW_P5 0x16 //The effective data width of receiving data channel 5 (1~32 bytes), set to 0 is illegal
+#define NRF_FIFO_STATUS 0x17 //FIFO status register; bit0, RX FIFO register empty flag; bit1, RX FIFO full flag; bit2, 3, reserved
+                              //bit4, TX FIFO empty flag; bit5, TX FIFO full flag; bit6, 1, cyclically send the last packet. 0, not cyclic;
+///////////////////////////////////////////////// ///////////////////////////////////////////////// //////
+//24L01 operation line
+#define NRF24L01_CE PDout(5) //24L01 chip select signal
+#define NRF24L01_CSN PDout(6) //SPI chip select signal
+#define NRF24L01_IRQ PDin(7) //IRQ host data input
 
-#define NRF24L01_2_CE     PDout(8)  //24L01片选信号
-#define NRF24L01_2_CSN    PDout(9)  //SPI片选信号
-#define NRF24L01_2_IRQ    PDin(10)   //IRQ主机数据输入
-//24L01发送接收数据宽度定义
-#define TX_ADR_WIDTH    5   	//5字节的地址宽度
-#define RX_ADR_WIDTH    5   	//5字节的地址宽度
-#define TX_PLOAD_WIDTH  32  	//32字节的用户数据宽度
-#define RX_PLOAD_WIDTH  32  	//32字节的用户数据宽度
-									   	   
+#define NRF24L01_2_CE PDout(8) //24L01 chip select signal
+#define NRF24L01_2_CSN PDout(9) //SPI chip select signal
+#define NRF24L01_2_IRQ PDin(10) //IRQ host data input
+//24L01 send and receive data width definition
+#define TX_ADR_WIDTH 5 //5 byte address width
+#define RX_ADR_WIDTH 5 //5 byte address width
+#define TX_PLOAD_WIDTH 32 //32-byte user data width
+#define RX_PLOAD_WIDTH 32 //32-byte user data width
 
-void NRF24L01_Init(void);//初始化
-void NRF24L01_2_Init(void);//初始化
 
-void NRF24L01_RX_Mode(void);//配置为接收模式
-void NRF24L01_2_RX_Mode(void);//配置为接收模式
+void NRF24L01_Init(void);//initialization
+void NRF24L01_2_Init(void);//initialization
 
-void NRF24L01_TX_Mode(void);//配置为发送模式
-void NRF24L01_2_TX_Mode(void);//配置为发送模式
+void NRF24L01_RX_Mode(void);//Configure to receive mode
+void NRF24L01_2_RX_Mode(void);//Configure to receive mode
 
-u8 NRF24L01_Write_Buf(u8 reg, u8 *pBuf, u8 u8s);//写数据区
-u8 NRF24L01_2_Write_Buf(u8 reg, u8 *pBuf, u8 u8s);//写数据区
+void NRF24L01_TX_Mode(void);//Configure to transmit mode
+void NRF24L01_2_TX_Mode(void);//Configure to transmit mode
 
-u8 NRF24L01_Read_Buf(u8 reg, u8 *pBuf, u8 u8s);//读数据区
-u8 NRF24L01_2_Read_Buf(u8 reg, u8 *pBuf, u8 u8s);//读数据区
+u8 NRF24L01_Write_Buf(u8 reg, u8 *pBuf, u8 u8s);//write data area
+u8 NRF24L01_2_Write_Buf(u8 reg, u8 *pBuf, u8 u8s);//write data area
 
-u8 NRF24L01_Read_Reg(u8 reg);			//读寄存器
-u8 NRF24L01_2_Read_Reg(u8 reg);			//读寄存器
+u8 NRF24L01_Read_Buf(u8 reg, u8 *pBuf, u8 u8s);//Read data area
+u8 NRF24L01_2_Read_Buf(u8 reg, u8 *pBuf, u8 u8s);//Read data area
 
-u8 NRF24L01_Write_Reg(u8 reg, u8 value);//写寄存器
-u8 NRF24L01_2_Write_Reg(u8 reg, u8 value);//写寄存器
+u8 NRF24L01_Read_Reg(u8 reg); //Read register
+u8 NRF24L01_2_Read_Reg(u8 reg); //Read register
 
-u8 NRF24L01_Check(void);//检查24L01是否存在
-u8 NRF24L01_2_Check(void);//检查24L01是否存在
+u8 NRF24L01_Write_Reg(u8 reg, u8 value);//write register
+u8 NRF24L01_2_Write_Reg(u8 reg, u8 value);//write register
 
-u8 NRF24L01_TxPacket(u8 *txbuf);//发送一个包的数据
-u8 NRF24L01_2_TxPacket(u8 *txbuf);//发送一个包的数据
+u8 NRF24L01_Check(void);//Check whether 24L01 exists
+u8 NRF24L01_2_Check(void);//Check whether 24L01 exists
 
-u8 NRF24L01_RxPacket(u8 *rxbuf);//接收一个包的数据
-u8 NRF24L01_2_RxPacket(u8 *rxbuf);//接收一个包的数据
+u8 NRF24L01_TxPacket(u8 *txbuf);//Send a packet of data
+u8 NRF24L01_2_TxPacket(u8 *txbuf);//Send a packet of data
+
+u8 NRF24L01_RxPacket(u8 *rxbuf);//Receive a packet of data
+u8 NRF24L01_2_RxPacket(u8 *rxbuf);//Receive a packet of data
 
 
 #endif
