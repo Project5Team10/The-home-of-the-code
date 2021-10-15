d35b0ea31fbceb16c9d0071080cc1a58b701b401
Zifeng Zhao(22942831) <89691293+NOanxiety@users.noreply.github.com>
2021-10-15 21:31:46 +0800

    Update can.h

--- STM32F407VE/HARDWARE/CAN/can.h
+++ STM32F407VE/HARDWARE/CAN/can.h
@@ -2,18 +2,18 @@
 #define __CAN_H
 #include "sys.h"
 
-//CAN1接收RX0中断使能
-#define CAN1_RX0_INT_ENABLE	0		//0,不使能;1,使能.
-#define CAN2_RX0_INT_ENABLE	0		//0,不使能;1,使能.
+//CAN1 receive RX0 interrupt enable
+#define CAN1_RX0_INT_ENABLE 0 //0, disable; 1, enable.
+#define CAN2_RX0_INT_ENABLE 0 //0, disable; 1, enable.
 
-u8 CAN1_Mode_Init(u32 tsjw,u32 tbs2,u32 tbs1,u16 brp,u32 mode);//CAN初始化
-u8 CAN2_Mode_Init(u32 tsjw,u32 tbs2,u32 tbs1,u16 brp,u32 mode);//CAN初始化
+u8 CAN1_Mode_Init(u32 tsjw,u32 tbs2,u32 tbs1,u16 brp,u32 mode);//CAN initialization
+u8 CAN2_Mode_Init(u32 tsjw,u32 tbs2,u32 tbs1,u16 brp,u32 mode);//CAN initialization
 
 void CAN_Config(void);
 void CAN2_Config(void);
-u8 CAN1_Send_Msg(u8* msg,u8 len);						//发送数据
-u8 CAN1_Receive_Msg(u8 *buf);							//接收数据
+u8 CAN1_Send_Msg(u8* msg,u8 len); //Send data
+u8 CAN1_Receive_Msg(u8 *buf); //Receive data
 
-u8 CAN2_Send_Msg(u8* msg,u8 len);						//发送数据
-u8 CAN2_Receive_Msg(u8 *buf);							//接收数据
+u8 CAN2_Send_Msg(u8* msg,u8 len); //Send data
+u8 CAN2_Receive_Msg(u8 *buf); //Receive data
 #endif
