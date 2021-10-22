afb1199fb1452e1baba07f9fdf2f7e7b4c22bd5e
Zifeng Zhao(22942831) <89691293+NOanxiety@users.noreply.github.com>
2021-10-22 11:08:16 +0800

    Update can.c

--- STM32F407VE/HARDWARE/CAN/can.c
+++ STM32F407VE/HARDWARE/CAN/can.c
@@ -1,107 +1,106 @@
 #include "can.h"
 #include "main.h"
 #include "stm32f4xx_hal_def.h"
-CAN_HandleTypeDef	CAN1_Handler;   //CAN1句柄
-CAN_TxHeaderTypeDef	TxHeader;      //发送
-CAN_RxHeaderTypeDef	RxHeader;      //接收
+CAN_HandleTypeDef CAN1_Handler; //CAN1 handle
+CAN_TxHeaderTypeDef TxHeader; //send
+CAN_RxHeaderTypeDef RxHeader; //Receive
 
-CAN_HandleTypeDef	CAN2_Handler;   //CAN1句柄
-CAN_TxHeaderTypeDef	Tx2Header;      //发送
-CAN_RxHeaderTypeDef	Rx2Header;      //接收
+CAN_HandleTypeDef CAN2_Handler; //CAN1 handle
+CAN_TxHeaderTypeDef Tx2Header; //send
+CAN_RxHeaderTypeDef Rx2Header; //Receive
 
-////CAN初始化
-//tsjw:重新同步跳跃时间单元.范围:CAN_SJW_1TQ~CAN_SJW_4TQ
-//tbs2:时间段2的时间单元.   范围:CAN_BS2_1TQ~CAN_BS2_8TQ;
-//tbs1:时间段1的时间单元.   范围:CAN_BS1_1TQ~CAN_BS1_16TQ
-//brp :波特率分频器.范围:1~1024; tq=(brp)*tpclk1
-//波特率=Fpclk1/((tbs1+tbs2+1)*brp); 其中tbs1和tbs2我们只用关注标识符上标志的序号，例如CAN_BS2_1TQ，我们就认为tbs2=1来计算即可。
-//mode:CAN_MODE_NORMAL,普通模式;CAN_MODE_LOOPBACK,回环模式;
-//Fpclk1的时钟在初始化的时候设置为42M,如果设置CAN1_Mode_Init(CAN_SJW_1tq,CAN_BS2_6tq,CAN_BS1_7tq,6,CAN_MODE_LOOPBACK);
-//则波特率为:42M/((6+7+1)*6)=500Kbps
-//返回值:0,初始化OK;
-//    其他,初始化失败;
+////CAN initialization
+//tsjw: Resynchronize the jump time unit. Range: CAN_SJW_1TQ~CAN_SJW_4TQ
+//tbs2: Time unit of time period 2. Range: CAN_BS2_1TQ~CAN_BS2_8TQ;
+//tbs1: Time unit of time period 1. Range: CAN_BS1_1TQ~CAN_BS1_16TQ
+//brp: baud rate divider. Range: 1~1024; tq=(brp)*tpclk1
+//Baud rate=Fpclk1/((tbs1+tbs2+1)*brp); Among them, tbs1 and tbs2 we only use the serial number marked on the identifier, such as CAN_BS2_1TQ, we think that tbs2=1 can be calculated.
+//mode: CAN_MODE_NORMAL, normal mode; CAN_MODE_LOOPBACK, loopback mode;
+//The clock of Fpclk1 is set to 42M during initialization, if CAN1_Mode_Init(CAN_SJW_1tq,CAN_BS2_6tq,CAN_BS1_7tq,6,CAN_MODE_LOOPBACK) is set;
+//The baud rate is: 42M/((6+7+1)*6)=500Kbps
+//Return value: 0, initialization is OK;
+// Other, initialization failed;
 
 u8 CAN1_Mode_Init(u32 tsjw,u32 tbs2,u32 tbs1,u16 brp,u32 mode)
 {
-	CAN_InitTypeDef		CAN1_InitConf;
+CAN_InitTypeDef CAN1_InitConf;
     
     CAN1_Handler.Instance=CAN1;
-	
-	CAN1_Handler.Init = CAN1_InitConf;
-	
-	 CAN1_Handler.Init.Prescaler=brp;				//分频系数(Fdiv)为brp+1
-	    CAN1_Handler.Init.Mode=mode;					//模式设置
-	    CAN1_Handler.Init.SyncJumpWidth=tsjw;			//重新同步跳跃宽度(Tsjw)为tsjw+1个时间单位 CAN_SJW_1TQ~CAN_SJW_4TQ
-	    CAN1_Handler.Init.TimeSeg1=tbs1;				//tbs1范围CAN_BS1_1TQ~CAN_BS1_16TQ
-	    CAN1_Handler.Init.TimeSeg2=tbs2;				//tbs2范围CAN_BS2_1TQ~CAN_BS2_8TQ
-	    CAN1_Handler.Init.TimeTriggeredMode=DISABLE;	//非时间触发通信模式
-	    CAN1_Handler.Init.AutoBusOff=DISABLE;			//软件自动离线管理
-	    CAN1_Handler.Init.AutoWakeUp=DISABLE;			//睡眠模式通过软件唤醒(清除CAN->MCR的SLEEP位)
-	    CAN1_Handler.Init.AutoRetransmission=ENABLE;	//禁止报文自动传送
-	    CAN1_Handler.Init.ReceiveFifoLocked=DISABLE;	//报文不锁定,新的覆盖旧的
-	    CAN1_Handler.Init.TransmitFifoPriority=DISABLE;	//优先级由报文标识符决定
-	
-    if(HAL_CAN_Init(&CAN1_Handler)!=HAL_OK)			//初始化
-		return 1;
+
+CAN1_Handler.Init = CAN1_InitConf;
+
+CAN1_Handler.Init.Prescaler=brp; //The frequency division coefficient (Fdiv) is brp+1
+CAN1_Handler.Init.Mode=mode; //Mode setting
+CAN1_Handler.Init.SyncJumpWidth=tsjw; //The resynchronization jump width (Tsjw) is tsjw+1 time unit CAN_SJW_1TQ~CAN_SJW_4TQ
+CAN1_Handler.Init.TimeSeg1=tbs1; //tbs1 range CAN_BS1_1TQ~CAN_BS1_16TQ
+CAN1_Handler.Init.TimeSeg2=tbs2; //tbs2 range CAN_BS2_1TQ~CAN_BS2_8TQ
+CAN1_Handler.Init.TimeTriggeredMode=DISABLE; //Non-time trigger communication mode
+CAN1_Handler.Init.AutoBusOff=DISABLE; //Software automatic offline management
+CAN1_Handler.Init.AutoWakeUp=DISABLE; //Sleep mode is awakened by software (clear the SLEEP bit of CAN->MCR)
+CAN1_Handler.Init.AutoRetransmission=ENABLE; //Prohibit automatic message transmission
+CAN1_Handler.Init.ReceiveFifoLocked=DISABLE; //The message is not locked, the new one overwrites the old one
+CAN1_Handler.Init.TransmitFifoPriority=DISABLE; //Priority is determined by the message identifier
+
+    if(HAL_CAN_Init(&CAN1_Handler)!=HAL_OK) //initialization
+return 1;
     return 0;
 }
 u8 CAN2_Mode_Init(u32 tsjw,u32 tbs2,u32 tbs1,u16 brp,u32 mode)
 {
-	CAN_InitTypeDef		CAN2_InitConf;
+CAN_InitTypeDef CAN2_InitConf;
     
     CAN2_Handler.Instance=CAN2;
-	
-	CAN2_Handler.Init = CAN2_InitConf;
-	
-	CAN2_Handler.Init.Prescaler=brp;				//分频系数(Fdiv)为brp+1
-	    CAN2_Handler.Init.Mode=mode;					//模式设置
-	    CAN2_Handler.Init.SyncJumpWidth=tsjw;			//重新同步跳跃宽度(Tsjw)为tsjw+1个时间单位 CAN_SJW_1TQ~CAN_SJW_4TQ
-	    CAN2_Handler.Init.TimeSeg1=tbs1;				//tbs1范围CAN_BS1_1TQ~CAN_BS1_16TQ
-	    CAN2_Handler.Init.TimeSeg2=tbs2;				//tbs2范围CAN_BS2_1TQ~CAN_BS2_8TQ
-	    CAN2_Handler.Init.TimeTriggeredMode=DISABLE;	//非时间触发通信模式
-	    CAN2_Handler.Init.AutoBusOff=DISABLE;			//软件自动离线管理
-	    CAN2_Handler.Init.AutoWakeUp=DISABLE;			//睡眠模式通过软件唤醒(清除CAN->MCR的SLEEP位)
-	    CAN2_Handler.Init.AutoRetransmission=ENABLE;	//禁止报文自动传送
-	    CAN2_Handler.Init.ReceiveFifoLocked=DISABLE;	//报文不锁定,新的覆盖旧的
-	    CAN2_Handler.Init.TransmitFifoPriority=DISABLE;	//优先级由报文标识符决定
-	
-    if(HAL_CAN_Init(&CAN2_Handler)!=HAL_OK)			//初始化
-		return 1;
+
+CAN2_Handler.Init = CAN2_InitConf;
+
+CAN2_Handler.Init.Prescaler=brp; //The frequency division coefficient (Fdiv) is brp+1
+CAN2_Handler.Init.Mode=mode; //Mode setting
+CAN2_Handler.Init.SyncJumpWidth=tsjw; //The resynchronization jump width (Tsjw) is tsjw+1 time unit CAN_SJW_1TQ~CAN_SJW_4TQ
+CAN2_Handler.Init.TimeSeg1=tbs1; //tbs1 range CAN_BS1_1TQ~CAN_BS1_16TQ
+CAN2_Handler.Init.TimeSeg2=tbs2; //tbs2 range CAN_BS2_1TQ~CAN_BS2_8TQ
+CAN2_Handler.Init.TimeTriggeredMode=DISABLE; //Non-time trigger communication mode
+CAN2_Handler.Init.AutoBusOff=DISABLE; //Software automatic offline management
+CAN2_Handler.Init.AutoWakeUp=DISABLE; //Sleep mode is awakened by software (clear the SLEEP bit of CAN->MCR)
+CAN2_Handler.Init.AutoRetransmission=ENABLE; //Prohibit automatic message transmission
+CAN2_Handler.Init.ReceiveFifoLocked=DISABLE; //The message is not locked, the new one overwrites the old one
+CAN2_Handler.Init.TransmitFifoPriority=DISABLE; //Priority is determined by the message identifier
+
+    if(HAL_CAN_Init(&CAN2_Handler)!=HAL_OK) //initialization
+return 1;
     return 0;
 }
-//CAN底层驱动，引脚配置，时钟配置，中断配置
-//此函数会被HAL_CAN_Init()调用
-//hcan:CAN句柄
+//CAN bottom layer driver, pin configuration, clock configuration, interrupt configuration
+//This function will be called by HAL_CAN_Init()
+//hcan: CAN handle
 /*void HAL_CAN_MspInit(CAN_HandleTypeDef* hcan)
 {
     GPIO_InitTypeDef GPIO_Initure;
     
-    __HAL_RCC_CAN1_CLK_ENABLE();                //使能CAN1时钟
-	__HAL_RCC_CAN2_CLK_ENABLE(); 
-    __HAL_RCC_GPIOD_CLK_ENABLE();			    //开启GPIOA时钟
-	__HAL_RCC_GPIOB_CLK_ENABLE();
-	
-    GPIO_Initure.Pin=GPIO_PIN_0|GPIO_PIN_1;   //PA11,12
-    GPIO_Initure.Mode=GPIO_MODE_AF_PP;          //推挽复用
-    GPIO_Initure.Pull=GPIO_PULLUP;              //上拉
-    GPIO_Initure.Speed=GPIO_SPEED_FAST;         //快速
-    GPIO_Initure.Alternate=GPIO_AF9_CAN1;       //复用为CAN1
-    HAL_GPIO_Init(GPIOD,&GPIO_Initure);         //初始化
+    __HAL_RCC_CAN1_CLK_ENABLE(); //Enable CAN1 clock
+__HAL_RCC_CAN2_CLK_ENABLE();
+    __HAL_RCC_GPIOD_CLK_ENABLE(); //Enable GPIOA clock
+__HAL_RCC_GPIOB_CLK_ENABLE();
 
-    GPIO_Initure.Pin=GPIO_PIN_12|GPIO_PIN_6;   //PA11,12
-    GPIO_Initure.Mode=GPIO_MODE_AF_PP;          //推挽复用
-    GPIO_Initure.Pull=GPIO_PULLUP;              //上拉
-    GPIO_Initure.Speed=GPIO_SPEED_FAST;         //快速
-    GPIO_Initure.Alternate=GPIO_AF9_CAN2;       //复用为CAN1
-    HAL_GPIO_Init(GPIOB,&GPIO_Initure);        //初始化
-	
+    GPIO_Initure.Pin=GPIO_PIN_0|GPIO_PIN_1; //PA11,12
+    GPIO_Initure.Mode=GPIO_MODE_AF_PP; //Push-pull multiplexing
+    GPIO_Initure.Pull=GPIO_PULLUP; //Pull up
+    GPIO_Initure.Speed=GPIO_SPEED_FAST; //Fast
+    GPIO_Initure.Alternate=GPIO_AF9_CAN1; //Multiplexed as CAN1
+    HAL_GPIO_Init(GPIOD,&GPIO_Initure); //initialization
+    GPIO_Initure.Pin=GPIO_PIN_12|GPIO_PIN_6; //PA11,12
+    GPIO_Initure.Mode=GPIO_MODE_AF_PP; //Push-pull multiplexing
+    GPIO_Initure.Pull=GPIO_PULLUP; //Pull up
+    GPIO_Initure.Speed=GPIO_SPEED_FAST; //Fast
+    GPIO_Initure.Alternate=GPIO_AF9_CAN2; //Multiplexed as CAN1
+    HAL_GPIO_Init(GPIOB,&GPIO_Initure); //initialization
+
 }*/
 
 void CAN_Config(void)
 {
-  CAN_FilterTypeDef  sFilterConfig;
+  CAN_FilterTypeDef sFilterConfig;
 
-  /*##-2- Configure the CAN Filter ###########################################*/
+  /*##-2- Configure the CAN Filter ##################################### ####*/
   sFilterConfig.FilterBank = 0;
   sFilterConfig.FilterMode = CAN_FILTERMODE_IDMASK;
   sFilterConfig.FilterScale = CAN_FILTERSCALE_32BIT;
@@ -117,26 +116,26 @@ void CAN_Config(void)
   {
     /* Filter configuration Error */
     while(1)
-	  {
-	  }
+{
+}
   }
-  /*##-3- Start the CAN peripheral ###########################################*/
+  /*##-3- Start the CAN peripheral #################################### ####*/
   if (HAL_CAN_Start(&CAN1_Handler) != HAL_OK)
   {
     /* Start Error */
     while(1)
-	  {
-	  }
+{
+}
   }
-  /*##-4- Activate CAN RX notification #######################################*/
+  /*##-4- Activate CAN RX notification ##################################### */
   if (HAL_CAN_ActivateNotification(&CAN1_Handler, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK)
   {
     /* Notification Error */
     while(1)
-	  {
-	  }
+{
+}
   }
-  /*##-5- Configure Transmission process #####################################*/
+  /*##-5- Configure Transmission process ###################################*/
   TxHeader.StdId = 0x321;
   TxHeader.ExtId = 0x01;
   TxHeader.RTR = CAN_RTR_DATA;
@@ -146,9 +145,9 @@ void CAN_Config(void)
 }
 void CAN2_Config(void)
 {
-  CAN_FilterTypeDef  sFilterConfig;
+  CAN_FilterTypeDef sFilterConfig;
 
-  /*##-2- Configure the CAN Filter ###########################################*/
+  /*##-2- Configure the CAN Filter ##################################### ####*/
   sFilterConfig.FilterBank = 0;
   sFilterConfig.FilterMode = CAN_FILTERMODE_IDMASK;
   sFilterConfig.FilterScale = CAN_FILTERSCALE_32BIT;
@@ -164,26 +163,26 @@ void CAN2_Config(void)
   {
     /* Filter configuration Error */
     while(1)
-	  {
-	  }
+{
+}
   }
-  /*##-3- Start the CAN peripheral ###########################################*/
+  /*##-3- Start the CAN peripheral #################################### ####*/
   if (HAL_CAN_Start(&CAN2_Handler) != HAL_OK)
   {
     /* Start Error */
     while(1)
-	  {
-	  }
+{
+}
   }
-  /*##-4- Activate CAN RX notification #######################################*/
+  /*##-4- Activate CAN RX notification ##################################### */
   if (HAL_CAN_ActivateNotification(&CAN2_Handler, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK)
   {
     /* Notification Error */
     while(1)
-	  {
-	  }
+{
+}
   }
-  /*##-5- Configure Transmission process #####################################*/
+  /*##-5- Configure Transmission process ###################################*/
   Tx2Header.StdId = 0x321;
   Tx2Header.ExtId = 0x01;
   Tx2Header.RTR = CAN_RTR_DATA;
@@ -191,90 +190,90 @@ void CAN2_Config(void)
   Tx2Header.DLC = 2;
   Tx2Header.TransmitGlobalTime = DISABLE;
 }
-//can发送一组数据(固定格式:ID为0X12,标准帧,数据帧)
-//len:数据长度(最大为8)
-//msg:数据指针,最大为8个字节.
-//返回值:0,成功;
-//		 其他,失败;
+//can send a set of data (fixed format: ID is 0X12, standard frame, data frame)
+//len: data length (maximum 8)
+//msg: data pointer, the maximum is 8 bytes.
+//Return value: 0, success;
+// Other, failure;
 u8 CAN1_Send_Msg(u8* msg,u8 len)
-{	
+{
     u8 i=0;
-	u32 TxMailbox;
-	u8 message[8];
-	  TxHeader.StdId=0X12;        //标准标识符
-	  TxHeader.ExtId=0x12;        //扩展标识符(29位)
-	  TxHeader.IDE=CAN_ID_STD;    //使用标准帧
-	  TxHeader.RTR=CAN_RTR_DATA;  //数据帧֡
-    TxHeader.DLC=len;                
+u32 TxMailbox;
+u8 message[8];
+TxHeader.StdId=0X12; //Standard identifier
+TxHeader.ExtId=0x12; //Extended identifier (29 bits)
+TxHeader.IDE=CAN_ID_STD; //Use standard frame
+TxHeader.RTR=CAN_RTR_DATA; //Data frame ֡
+    TxHeader.DLC=len;
     for(i=0;i<len;i++)
     {
-		message[i]=msg[i];
-	}
-    if(HAL_CAN_AddTxMessage(&CAN1_Handler, &TxHeader, message, &TxMailbox) != HAL_OK)//发送
-	{
-		return 1;
-	}
-	while(HAL_CAN_GetTxMailboxesFreeLevel(&CAN1_Handler) != 3) {}
+message[i]=msg[i];
+}
+    if(HAL_CAN_AddTxMessage(&CAN1_Handler, &TxHeader, message, &TxMailbox) != HAL_OK)//Send
+{
+return 1;
+}
+while(HAL_CAN_GetTxMailboxesFreeLevel(&CAN1_Handler) != 3) {}
     return 0;
 }
 u8 CAN2_Send_Msg(u8* msg,u8 len)
-{	
+{
     u8 i=0;
-	u32 TxMailbox;
-	u8 message[8];
-	Tx2Header.StdId=0X12;        //标准标识符
-	    Tx2Header.ExtId=0x12;        //扩展标识符(29位)
-	    Tx2Header.IDE=CAN_ID_STD;    //使用标准帧
-	    Tx2Header.RTR=CAN_RTR_DATA;  //数据帧֡
-    Tx2Header.DLC=len;                
+u32 TxMailbox;
+u8 message[8];
+Tx2Header.StdId=0X12; //Standard identifier
+Tx2Header.ExtId=0x12; //Extended identifier (29 bits)
+Tx2Header.IDE=CAN_ID_STD; //Use standard frame
+Tx2Header.RTR=CAN_RTR_DATA; //Data frame ֡
+    Tx2Header.DLC=len;
     for(i=0;i<len;i++)
     {
-		message[i]=msg[i];
-	}
-    if(HAL_CAN_AddTxMessage(&CAN2_Handler, &Tx2Header, message, &TxMailbox) != HAL_OK)//发送
-	{
-		return 1;
-	}
-	while(HAL_CAN_GetTxMailboxesFreeLevel(&CAN2_Handler) != 3) {}
+message[i]=msg[i];
+}
+    if(HAL_CAN_AddTxMessage(&CAN2_Handler, &Tx2Header, message, &TxMailbox) != HAL_OK)//Send
+{
+return 1;
+}
+while(HAL_CAN_GetTxMailboxesFreeLevel(&CAN2_Handler) != 3) {}
     return 0;
 }
-//can口接收数据查询
-//buf:数据缓存区;
-//返回值:0,无数据被收到;
-//		 其他,接收的数据长度;
+//can port receive data query
+//buf: data buffer area;
+//Return value: 0, no data was received;
+// Other, received data length;
 u8 CAN1_Receive_Msg(u8 *buf)
 {
- 	u32 i;
-	u8	RxData[8];
+ u32 i;
+u8 RxData[8];
 
-	if(HAL_CAN_GetRxFifoFillLevel(&CAN1_Handler, CAN_RX_FIFO0) != 1)
-	{
-		return 0xF1;
-	}
+if(HAL_CAN_GetRxFifoFillLevel(&CAN1_Handler, CAN_RX_FIFO0) != 1)
+{
+return 0xF1;
+}
 
-	if(HAL_CAN_GetRxMessage(&CAN1_Handler, CAN_RX_FIFO0, &RxHeader, RxData) != HAL_OK)
-	{
-		return 0xF2;
-	}
+if(HAL_CAN_GetRxMessage(&CAN1_Handler, CAN_RX_FIFO0, &RxHeader, RxData) != HAL_OK)
+{
+return 0xF2;
+}
     for(i=0;i<RxHeader.DLC;i++)
     buf[i]=RxData[i];
-	return RxHeader.DLC;
+return RxHeader.DLC;
 }
 u8 CAN2_Receive_Msg(u8 *buf)
 {
- 	u32 i;
-	u8	RxData[8];
+ u32 i;
+u8 RxData[8];
 
-	if(HAL_CAN_GetRxFifoFillLevel(&CAN2_Handler, CAN_RX_FIFO0) != 1)
-	{
-		return 0xF1;
-	}
+if(HAL_CAN_GetRxFifoFillLevel(&CAN2_Handler, CAN_RX_FIFO0) != 1)
+{
+return 0xF1;
+}
 
-	if(HAL_CAN_GetRxMessage(&CAN2_Handler, CAN_RX_FIFO0, &Rx2Header, RxData) != HAL_OK)
-	{
-		return 0xF2;
-	}
+if(HAL_CAN_GetRxMessage(&CAN2_Handler, CAN_RX_FIFO0, &Rx2Header, RxData) != HAL_OK)
+{
+return 0xF2;
+}
     for(i=0;i<Rx2Header.DLC;i++)
     buf[i]=RxData[i];
-	return Rx2Header.DLC;
+return Rx2Header.DLC;
 }
